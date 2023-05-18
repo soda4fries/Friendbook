c7a7fd549eedd5c1dbbc813407a7360de8051caf
soda4fries <39405916+soda4fries@users.noreply.github.com>
2023-05-18 20:50:11 +0800

    db config

---
+++ src/test/java/com/wia1002g3/friendbook/FriendBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wia1002g3.friendbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
