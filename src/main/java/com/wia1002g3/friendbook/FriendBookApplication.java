68dcf5fa71cb0a330350930343393d618170e5cf
soda4fries <39405916+soda4fries@users.noreply.github.com>
2023-05-20 01:49:56 +0800

    db config

--- src/main/java/com/wia1002g3/friendbook/FriendBookApplication.java
+++ src/main/java/com/wia1002g3/friendbook/FriendBookApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @SpringBootApplication
 public class FriendBookApplication {
@@ -15,5 +16,7 @@ public class FriendBookApplication {
     @GetMapping("/")
     public String test(){
         return "hello";
+        
     }
+
 }
