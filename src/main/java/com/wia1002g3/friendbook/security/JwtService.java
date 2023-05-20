7849568cf4c19906358d39a1d57a9ef23065718a
soda4fries <39405916+soda4fries@users.noreply.github.com>
2023-05-21 01:58:44 +0800

    Adding JWT auth

---
+++ src/main/java/com/wia1002g3/friendbook/security/JwtService.java
@@ -0,0 +1,13 @@
+package com.wia1002g3.friendbook.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+        return null;
+    }
+
+
+
+}
