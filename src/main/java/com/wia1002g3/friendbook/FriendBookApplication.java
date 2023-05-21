f63b7aa6c76c9771ab6c5bbce015872763518fa2
Nicolas Perera Niru Roy <116193503+nicolasroyy@users.noreply.github.com>
2023-05-21 16:49:15 +0800

    test

---
+++ src/main/java/com/wia1002g3/friendbook/FriendBookApplication.java
@@ -0,0 +1,22 @@
+package com.wia1002g3.friendbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class FriendBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendBookApplication.class, args);
+    }
+    @GetMapping("/")
+    public String test(){
+        return "hello";
+        
+    }
+
+}
---
+++ src/main/java/com/wia1002g3/friendbook/entity/User.java
@@ -0,0 +1,55 @@
+package com.wia1002g3.friendbook.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.List;
+
+import java.util.Collection;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
